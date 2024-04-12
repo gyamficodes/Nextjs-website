998aad3420b0387de49aded7de4f8678c333e899
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-04-12 13:48:52 -0700

    offer page created

--- next-project/components/Offer/Offer.jsx
+++ next-project/components/Offer/Offer.jsx
@@ -16,20 +16,21 @@ const Offer = () => {
     {
       id: 2,
       image:  `/offer/offer2.jpg`,
-      title: "The Green Building",
+      title: "house renovation",
       text: "Nulla iaculis turpis in nibh aliquam maximus. In dignissim arcu vel diam scelerisque, pretium urna ",
       btn: "read more",
     },
     {
       id: 1,
       image: `/offer/offer3.jpg`,
-      title: "The Green Building",
+      title: "construction $ design",
       text: "Nulla iaculis turpis in nibh aliquam maximus. In dignissim arcu vel diam scelerisque, pretium urna ",
       btn: "read more",
     },
   ];
 
 
+  
   
   return (
     <>
@@ -42,13 +43,18 @@ const Offer = () => {
             <div className="w-[50px] h-[2px] bg-const-btn  mt-2"></div>
           </div>
 
-          <div className=" grid grid-cols-3 w-full mt-[70px]" >
-              <div className=" w-full h-auto">
-               <img className="  w-full h-[220px]" src="/offer/offer1.jpg" alt="image" />
-               <h1 className=" text-[20px] text-[#4b4b4b] my-5  capitalize  cursor-pointer  hover:text-const-btn">the green building </h1>
-               <p className=" text-[14px] text-[#7777] mt-4">Nulla iaculis turpis in nibh aliquam maximus. In dignissim arcu vel diam scelerisque, pretium urna</p>
-               <button className=" w-[170px] hover:bg-const-nav  h-[50px] bg-const-btn capitalize mt-5   text-white transition-all ease-in-out duration-100">read more</button>
-              </div>
+          <div className=" grid grid-cols-3 w-full mt-[70px] gap-4" >
+          {items.map(item  => (
+                     <div key={item.id} className=" w-full h-auto">
+                     <img className="  w-full h-[220px]" src={item.image} alt="image" />
+                     <h1 className=" text-[20px] text-[#4b4b4b] my-5  capitalize  cursor-pointer  hover:text-const-btn">{item.title} </h1>
+                     <p className=" text-[14px] text-[#7777] mt-4">{item.text}</p>
+                     <button className=" w-[170px] hover:bg-const-nav  h-[50px] bg-const-btn capitalize mt-5   text-white transition-all ease-in-out duration-100">{item.btn}</button>
+                    </div>
+                  ))
+
+                  }
+             
           </div>
         </div>
       </section>
