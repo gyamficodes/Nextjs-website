897d77649b7e9bda0fb2d1e536e1e27cf34c05b4
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-04-17 23:40:29 -0700

    testimonial page started

--- next-project/components/About/About.jsx
+++ next-project/components/About/About.jsx
@@ -30,8 +30,8 @@ const images = [
             <div className=" grid lg:grid-cols-2 gap-5 w-full">
                 <div>
             <div className=" mb-7">
-<h1 className=" text-[22px] font-[300]  text-const-text-p">WHY CHOOSE US?</h1>
-<div className="h-[2px] mt-3 bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
+<h1 className=" text-[22px] font-[300]  text-const-text-p uppercase">WHY CHOOSE US?</h1>
+<div className="h-[3px] mt-3 bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
  before:bg-const-btn before:h-full before:w-[10%] before:absolute before:z-[-1]
 "></div>
             </div>
@@ -39,8 +39,8 @@ const images = [
                 <Accordion items={accordionItems} />
                 </div>
                 <div>
-                <h1 className=" text-[25px] font-[300]  text-const-text-p capitalize">About us</h1>
-<div className="h-[2px] mt-3  bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
+                <h1 className=" text-[25px] font-[300]  text-const-text-p   uppercase">About us</h1>
+<div className="h-[3px] mt-3  bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
  before:bg-const-btn before:h-full before:w-[10%] before:absolute before:z-[-1]
 "></div>
 <div className=" mt-8">
--- next-project/components/Testimonial/Testimonial.jsx
+++ next-project/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,15 @@
 
 
 const Testimonial = () => {
+const img =  [
+    {id:1, url:`/test/test1.png`},
+    { id:2, url:`/test/test2.png`},
+    { id:3,url:`/test/test2.png`},
+    { id:4,url:`/test/test4.png`},
+    { id:5,url:`/test/test5.png`},
+    { id:6,url:`/test/test6.png`},
+];
+
   return (
      <>
     <section className=" h-auto bg-const-text py-[60px]">
@@ -8,20 +17,29 @@ const Testimonial = () => {
             <div className=" grid lg:grid-cols-2 gap-5 w-full">
                 <div>
             <div className=" mb-7">
-<h1 className=" text-[22px] font-[300]  text-const-text-p">WHY CHOOSE US?</h1>
-<div className="h-[2px] mt-3 bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
+<h1 className=" text-[22px] font-[300]  text-const-text-p uppercase">our partners</h1>
+<div className="h-[3px] mt-3 bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
  before:bg-const-btn before:h-full before:w-[10%] before:absolute before:z-[-1]
 "></div>
+
+
+<div className="  grid grid-cols-3 w-full gap-3 mt-[50px]">
+     { img.map((item) => (
+         <img key={item.id} src={item.url} alt="pic" />
+     ))
+
+     }
+</div>
+             
             </div>
 
-             
                 </div>
                 <div>
-                <h1 className=" text-[25px] font-[300]  text-const-text-p capitalize">About us</h1>
-<div className="h-[2px] mt-3  bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
+                <h1 className=" text-[25px] font-[300]  text-const-text-p  uppercase">TESTIMONIALS</h1>
+<div className="h-[3px] mt-3  bg-[#ccc] w-full relative z-[1] before:content-[''] before:top-0 before:left-0  before:bottom-0
  before:bg-const-btn before:h-full before:w-[10%] before:absolute before:z-[-1]
 "></div>
-
+zcvxfgbf
                 </div>
             </div>
         </div>
